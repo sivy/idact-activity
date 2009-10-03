5c88f5f7e65a10c95525af0369413b5a63b354fd
Mark Paschal <markpasc@markpasc.org>
2009-10-03 13:20:31 -0700

    Copy OpenID views (and some auth decorators) from another project

---
+++ activity/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+
+def auth_forbidden(fn):
+    @wraps(fn)
+    def check_for_anon(request, *args, **kwargs):
+        if request.user is None:
+            return fn(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('home'))
+
+    return check_for_anon
+
+
+def auth_required(fn):
+    @wraps(fn)
+    def check_for_auth(request, *args, **kwargs):
+        if request.user is None:
+            return HttpResponseRedirect(reverse('signin'))
+        return fn(request, *args, **kwargs)
+
+    return check_for_auth
+
+
+def admin_only(fn):
+    @wraps(fn)
+    def check_for_admin(request, *args, **kwargs):
+        if getattr(request.user, 'is_admin', False):
+            return fn(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('home'))
+
+    return check_for_admin
--- activity/views.py
+++ activity/views.py
@@ -1,1 +1,67 @@
-# Create your views here.
+import logging
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from openid.consumer import consumer, discover
+from openid.extensions import sreg, ax
+
+from activity.decorators import auth_forbidden
+from activity.models import OpenIDStore
+
+
+log = logging.getLogger(__name__)
+
+
+# OpenID views
+
+@auth_forbidden
+def start(request):
+    openid_url = request.POST.get('openid_url', None)
+    if not openid_url:
+        username = request.POST.get('openid_username', None)
+        pattern = request.POST.get('openid_pattern', None)
+        if username and pattern:
+            openid_url = pattern.replace('{name}', username)
+    if not openid_url:
+        request.flash.put(loginerror="An OpenID as whom to sign in is required.")
+        return HttpResponseRedirect(reverse('signin'))
+    log.debug('Attempting to sign viewer in as %r', openid_url)
+
+    csr = consumer.Consumer(request.session, OpenIDStore())
+    try:
+        ar = csr.begin(openid_url)
+    except discover.DiscoveryFailure, exc:
+        request.flash.put(error=exc.message)
+        return HttpResponseRedirect(reverse('signin'))
+
+    # Ask for some stuff by sreg (in case it's supported).
+    ar.addExtension(sreg.SRegRequest(optional=('nickname', 'fullname', 'email')))
+
+    # Ask for some stuff by Attribute Exchange (for google).
+    fr = ax.FetchRequest()
+    fr.add(ax.AttrInfo("http://axschema.org/namePerson/first", alias='firstname', required=True))
+    fr.add(ax.AttrInfo("http://axschema.org/namePerson/last", alias='lastname'))
+    fr.add(ax.AttrInfo("http://axschema.org/contact/email", alias='email', required=True))
+    ar.addExtension(fr)
+
+    def whole_reverse(view):
+        return request.build_absolute_uri(reverse(view))
+
+    return_to = whole_reverse('library.auth.views.regular.complete')
+    redirect_url = ar.redirectURL(whole_reverse('home'), return_to)
+    return HttpResponseRedirect(redirect_url)
+
+
+@auth_forbidden
+def complete(request):
+    csr = consumer.Consumer(request.session, OpenIDStore())
+    resp = csr.complete(request.GET, request.build_absolute_uri())
+    if isinstance(resp, consumer.CancelResponse):
+        return HttpResponseRedirect(reverse('home'))
+    elif isinstance(resp, consumer.FailureResponse):
+        request.flash.put(error=resp.message)
+        return HttpResponseRedirect(reverse('signin'))
+    elif isinstance(resp, consumer.SuccessResponse):
+        OpenIDStore.make_person_from_response(resp)
+        request.session['openid'] = resp.identity_url
+        return HttpResponseRedirect(reverse('home'))
