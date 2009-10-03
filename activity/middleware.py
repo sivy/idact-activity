9827f0b84608b4b83b8af214f16eb06088f419ef
Mark Paschal <markpasc@markpasc.org>
2009-10-03 14:54:02 -0700

    Add logging to try to figure out why you don't seem signed in after signing in

--- activity/middleware.py
+++ activity/middleware.py
@@ -1,18 +1,29 @@
+import logging
+
 from activity.models import Person
+
+
+log = logging.getLogger(__name__)
 
 
 class AuthenticationMiddleware(object):
 
     def process_request(self, request):
         person = None
+        sessionid = request.COOKIES['sessionid']
 
         try:
             openid = request.session['openid']
             person = Person.objects.get(openid=openid)
         except KeyError:
-            pass
+            log.debug('Visitor %r has no openid', sessionid)
         except Person.DoesNotExist:
+            log.debug('Visitor %r had openid %r but no Person record',
+                sessionid, openid)
             # Have them sign in again.
             del request.session['openid']
+        else:
+            log.debug('Visitor %r has openid %r which is %r',
+                sessionid, openid, person)
 
         request.user = person
